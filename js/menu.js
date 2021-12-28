b85243b7d17b725a19103b5b82b63a57cab05665
danbohdan <81313913+danxczm@users.noreply.github.com>
2021-12-28 17:27:38 +0200

    hw-08
    first draft

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector('.header-menu__button'),
+      closeMenuBtn: document.querySelector('.mobile-menu__button'),
+      menu: document.querySelector('.mobile-menu'),
+    };
+  
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-hidden');
+    }
+  })();
+  
